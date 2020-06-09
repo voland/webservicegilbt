090e388948ade84805e5e66b858afa814c0fc0ee
Arkadiusz Gil <voland83@gmail.com>
2020-06-09 10:58:57 +0200

    added firmware.cs also used for files

--- Server/Shared/Firmware.cs
+++ Server/Shared/Firmware.cs
@@ -1,54 +1,43 @@
 using System;
 using System.IO;
 
-namespace WebServiceGilBT.Shared
-{
-    public class Firmware
-    {
+namespace WebServiceGilBT.Shared {
+    public class Firmware {
         public int uid { set; get; }
         public int datasize { set; get; }
         public string filename { set; get; }
         public byte[] data { set; get; }
         public int checksum { set; get; }
-        public Firmware(String argFileName)
-        {
+        private string path;
+        public Firmware(String argFileName) {
             data = new byte[] { 121, 2, 3, 4, 5, 6, 7 };
-            filename = argFileName;
+            path = argFileName;
+            filename = Path.GetFileName(path);
             checksum = 0;
             LoadFromFile();
-            foreach (byte b in data)
-            {
+            foreach (byte b in data) {
                 checksum += (int)b;
             }
             datasize = data.Length;
         }
 
-        public void SaveToFile()
-        {
-            using (FileStream fs = new FileStream(filename, FileMode.Create))
-            {
-                using (BinaryWriter bw = new BinaryWriter(fs))
-                {
+        public void SaveToFile() {
+            using (FileStream fs = new FileStream(path, FileMode.Create)) {
+                using (BinaryWriter bw = new BinaryWriter(fs)) {
                     bw.Write(data);
                 }
             }
         }
 
-        private void LoadFromFile()
-        {
-            try
-            {
-                using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
-                {
-                    using (BinaryReader br = new BinaryReader(fs))
-                    {
-                        long len = new FileInfo(filename).Length;
+        private void LoadFromFile() {
+            try {
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read)) {
+                    using (BinaryReader br = new BinaryReader(fs)) {
+                        long len = new FileInfo(path).Length;
                         data = br.ReadBytes((int)len);
                     }
                 }
-            }
-            catch
-            {
+            } catch {
             }
         }
     }
