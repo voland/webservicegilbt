8c0f9d34419a9cf58bf7afac35c11efa5d2eec12
Arkadiusz Gil <voland83@gmail.com>
2020-09-07 15:55:43 +0200

    warn removed

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WebServiceGilBT.Data;
-using WebServiceGilBT.Shared;
+/* using WebServiceGilBT.Shared; */
 using Microsoft.AspNetCore.Components.Authorization;
 using Blazored.SessionStorage;
 /* using Microsoft.JSInterop; */
