55f6bcb56300f6314ae7551ccba64f90724fd659
Arkadiusz Gil <voland83@gmail.com>
2020-10-29 16:06:18 +0100

    some reformat to last commit

--- Server/Services/UniversalMsqlService.cs
+++ Server/Services/UniversalMsqlService.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using WebServiceGilBT.Data;
 
 namespace WebServiceGilBT.Services {
     public class UniversalMysqlService<T> {
@@ -20,10 +17,10 @@ namespace WebServiceGilBT.Services {
             this.tabelaName = tableName;
             IdName = PrimaryKeyPropertyName;
             PropertyInfo[] propertyInfos;
-            propertyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance );
+            propertyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
             propertyNames = new List<string>();
-            foreach(PropertyInfo pi in propertyInfos) {
-                if(pi.Name!=IdName) {
+            foreach (PropertyInfo pi in propertyInfos) {
+                if (pi.Name != IdName) {
                     propertyNames.Add(pi.Name);
                 }
             }
@@ -37,16 +34,8 @@ namespace WebServiceGilBT.Services {
 
         public async Task<List<T>> GetAllRecordsAsync() {
             string sql = "select * from " + tabelaName;
-
             return await _db.LoadData<T, dynamic>(sql, new { });
         }
-
-        //public async Task<List<T>> GetRecordsWithoutDuplicationBy1ParameterAsync<T>() {
-        //    string sql = string.Format(@" SELECT DISTINCT {1}
-        //    FROM {0}", tabelaName, property1Name);
-
-        //    return await _db.LoadData<T, dynamic>(sql, new { });
-        //}
 
         public async Task<T> GetRecordByIdAsync(int id) {
             string sql = string.Format("select * from {0} where {1}={2}", tabelaName, IdName, id);
@@ -55,38 +44,34 @@ namespace WebServiceGilBT.Services {
                     return listaPrzejsciowa[0];
             return default;
         }
-        public async Task PostRecordAsync(T argS) {
 
+        public async Task PostRecordAsync(T argS) {
             string names = "";
             string values = "";
-
             foreach (string s in propertyNames) {
                 names += s + " , ";
                 values += $"@{s} , ";
             }
             names = names.Substring(0, names.Length - 2);
             values = values.Substring(0, values.Length - 2);
-
             string sql = string.Format(@"insert into {0} ({1})
                            values ({2});", tabelaName, names, values);
-
             await _db.SaveDataAsync(sql, argS);
         }
 
-        public async Task UpdateRecordAsync(T argS , int id) {
+        public async Task UpdateRecordAsync(T argS, int id) {
             string sets = "";
             foreach (string s in propertyNames) {
                 sets += $"{s} = @{s} , ";
             }
             sets = sets.Substring(0, sets.Length - 2);
-
             string sql = string.Format(@" UPDATE {0}
                         SET {1}
                         WHERE {2} = {3} ", tabelaName, sets, IdName, id);
             await _db.SaveDataAsync(sql, argS);
         }
 
-        public async Task UpdateCertainProperiesInRecordAsync(T argS,List<string>listOfPropertyNames, int id) {
+        public async Task UpdateCertainProperiesInRecordAsync(T argS, List<string> listOfPropertyNames, int id) {
             string sets = "";
             foreach (string s in listOfPropertyNames) {
                 sets += $"{s} = @{s} , ";
