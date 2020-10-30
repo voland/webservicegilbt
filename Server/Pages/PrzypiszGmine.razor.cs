a8a49748d1555cb64fe5798822b4cd47e30dfcba
Arkadiusz Gil <voland83@gmail.com>
2020-10-30 09:27:02 +0100

    adde przypiszgmine

---
+++ Server/Pages/PrzypiszGmine.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebServiceGilBT.Data;
+using WebServiceGilBT.Services;
+
+namespace WebServiceGilBT.Pages {
+    public partial class PrzypiszGmine : ComponentBase {
+        public List<User> _userlist;
+
+        [Inject]
+        Blazored.SessionStorage.ISessionStorageService _sessionStorageService { set; get; }
+
+        [Inject]
+        UserMySQLService userService { set; get; }
+
+		[Inject]
+        ScreenListMySQLService scrLstSrvc {set; get;}
+
+        protected async override Task OnInitializedAsync() {
+            _userlist = await userService.GetUserListAsync();
+            loggeduser = await GetLoggedUser();
+
+        }
+
+        protected async Task<User> GetLoggedUser() {
+            string serializedUser = await _sessionStorageService.GetItemAsync<string>("loggedUser");
+            User u = null;
+            if (serializedUser != null)
+                u = JsonSerializer.Deserialize<User>(serializedUser);
+            if (u == null) {
+                u = new User();
+            }
+            return u;
+        }
+
+        private User _u = null;
+        public User loggeduser {
+            set { _u = value; }
+            get {
+                if (_u == null) _u = new User();
+                return _u;
+            }
+        }
+
+    }
+}
