d39566b07fc3d8225719057b455eaed15ca45bde
Arkadiusz Gil <voland83@gmail.com>
2020-10-30 10:39:16 +0100

    pageeditor commit

--- Server/Shared/PresentationEditor.razor.cs
+++ Server/Shared/PresentationEditor.razor.cs
@@ -1,12 +1,22 @@
 using System;
-
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Routing.Constraints;
+using WebServiceGilBT.Data;
+using WebServiceGilBT.Services;
 
 namespace WebServiceGilBT.Shared {
-    public partial class PresentationEditor : ComponentBase {
+    public partial class PresentationEditor : ComponentBase, IDisposable {
         [Parameter]
         public Pres Pres { set; get; }
 
+        [Parameter]
+        public Screen scr { get; set; }
+
+        [Parameter]
+        public User loggeduser { get; set; }
+
+        UniversalMysqlService<PresTemplate> presTemplateService;
         //element edit
         public void MoveUp(Page p) {
             int idx = Pres.pages.IndexOf(p);
@@ -62,5 +72,93 @@ namespace WebServiceGilBT.Shared {
             }
         }
 
+        bool chooseTeamplate = false;
+        public void AddNameTemplateClicked() {
+            chooseTeamplate = false;
+            Page p = new Page(10);
+            p.elements.Add(PageElement.NewText(scr.name, 0, 0, 1, FontType.fontnormal8px));
+            p.elements.Add(PageElement.NewUid(0, 8, 1, FontType.fontfat8px));
+            Pres.pages.Add(p);
+        }
+
+        public void AddDateTimeTemplateClicked() {
+            chooseTeamplate = false;
+            Page p = new Page(10);
+            p.elements.Add(PageElement.NewText("Godzina Data", 0, 0, 1, FontType.fontfat8px));
+            p.elements.Add(PageElement.NewTime(0, 8, 1, FontType.fontfat8px));
+            p.elements.Add(PageElement.NewDate(40, 8, 1, FontType.fontfat8px));
+            Pres.pages.Add(p);
+        }
+
+        public void AddPM10TemplateClicked() {
+            chooseTeamplate = false;
+            Page p = new Page(10);
+            p.elements.Add(PageElement.NewText("Pm10", 0, 0, 1, FontType.fontfat8px));
+            p.elements.Add(PageElement.NewSensorPm10(Pres.UnifiedIdx, 35, 0, 1, FontType.fontfat8px));
+            p.elements.Add(PageElement.NewText("Pm10", 0, 8, 1, FontType.fontfat8px));
+            PageElement pe = PageElement.NewSensorPm10(Pres.UnifiedIdx, 35, 8, 1, FontType.fontfat8px);
+            pe.type = ElementType.SENSOR_PM10_PERCENT;
+            p.elements.Add(pe);
+            Pres.pages.Add(p);
+        }
+
+        public void AddPM2_5TemplateClicked() {
+            chooseTeamplate = false;
+            Page p = new Page(10);
+            p.elements.Add(PageElement.NewText("Pm2.5", 0, 0, 1, FontType.fontfat8px));
+            p.elements.Add(PageElement.NewSensorPm2_5(Pres.UnifiedIdx, 35, 0, 1, FontType.fontfat8px));
+            p.elements.Add(PageElement.NewText("Pm2.5", 0, 8, 1, FontType.fontfat8px));
+            PageElement pe = PageElement.NewSensorPm2_5(Pres.UnifiedIdx, 35, 8, 1, FontType.fontfat8px);
+            pe.type = ElementType.SENSOR_PM2_5_PERCENT;
+            p.elements.Add(pe);
+            Pres.pages.Add(p);
+        }
+
+
+
+        public void AddPreassureTempTemplateClicked() {
+            chooseTeamplate = false;
+            Page p = new Page(10);
+            p.elements.Add(PageElement.NewText("Ciśnie.", 0, 0, 1, FontType.fontfat8px));
+            p.elements.Add(PageElement.NewSensorPressure(Pres.UnifiedIdx, 35, 0, 1, FontType.fontfat8px));
+            p.elements.Add(PageElement.NewText("Temp.", 0, 8, 1, FontType.fontfat8px));
+            PageElement pe = PageElement.NewSensorTemperature(Pres.UnifiedIdx, 35, 8, 1, FontType.fontfat8px);
+            p.elements.Add(pe);
+            Pres.pages.Add(p);
+        }
+
+        public void AddAirQualityTemplateClicked() {
+            chooseTeamplate = false;
+            Page p = new Page(10);
+            p.elements.Add(PageElement.NewText("Jakość powietrza", 0, 0, 1, FontType.fontfat8px));
+            PageElement pe = PageElement.NewSensorPm10(Pres.UnifiedIdx, 0, 8, 1, FontType.fontfat8px);
+            pe.type = ElementType.SENSOR_PM10_STATUS;
+            p.elements.Add(pe);
+            Pres.pages.Add(p);
+        }
+
+        protected override void OnInitialized() {
+            Lang.LangChanged += StateHasChanged;
+            base.OnInitialized();
+        }
+        public void Dispose() {
+            Lang.LangChanged -= StateHasChanged;
+        }
+
+        bool showTypeTemplateName = false;
+        string templateName = "";
+        string templateKomunikat = "";
+        public async Task SaveAsTemplateClicked() {
+            if (!string.IsNullOrEmpty(templateName)) {
+                showTypeTemplateName = false;
+                if (presTemplateService == null) presTemplateService = new UniversalMysqlService<PresTemplate>(new SqlDataAccess(null), PresTemplate.tableName, nameof(PresTemplate.Id));
+                string data = DateTime.Now.ToString("G");
+                PresTemplate pt = new PresTemplate() { TemplateName = templateName, Width = scr.width, Height = scr.height, prezentacja = Pres, ScreenType = scr.screen_type, UserAuthorId = loggeduser.UserId, CreateDate = data };
+                await presTemplateService.PostRecordAsync(pt);
+                templateKomunikat = Lang.templateHasBeenSaved;
+            } else {
+                templateKomunikat = Lang.templateNameIsEmpty;
+            }
+        }
     }
 }
